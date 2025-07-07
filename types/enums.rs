b6a5ea51a123d8ad0506dd5697420c33f8ba0771
gregfc95 <josegfc95@gmail.com>
2025-07-07 00:03:00 -0300

    feat: add basic enums structure

---
+++ types/enums.rs
@@ -0,0 +1,28 @@
+use ink::scale::{Decode, Encode};
+
+#[derive(Debug, Clone, PartialEq, Eq, Encode, Decode)]
+#[cfg_attr(feature = "std", derive(ink::scale_info::TypeInfo))]
+pub enum Rol {
+    Comprador,
+    Vendedor,
+    Ambos,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Encode, Decode)]
+#[cfg_attr(feature = "std", derive(ink::scale_info::TypeInfo))]
+pub enum Categoria {
+    Tecnologia,
+    Indumentaria,
+    Hogar,
+    Alimentos,
+    Otros,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Encode, Decode)]
+#[cfg_attr(feature = "std", derive(ink::scale_info::TypeInfo))]
+pub enum Estado {
+    Pendiente,
+    Enviado,
+    Recibido,
+    Cancelada,
+}
