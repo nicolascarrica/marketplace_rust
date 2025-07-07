28d3335ec7382fe67dfe200225dc07a1da72be00
gregfc95 <josegfc95@gmail.com>
2025-07-07 00:13:45 -0300

    feat: add basic mod structure

---
+++ types/mod.rs
@@ -0,0 +1,11 @@
+pub mod enums;
+pub mod orden;
+pub mod producto;
+pub mod publicacion;
+pub mod usuario;
+
+pub use enums::*;
+pub use orden::*;
+pub use producto::*;
+pub use publicacion::*;
+pub use usuario::*;
