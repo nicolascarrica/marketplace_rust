05d71a10052ddb5c8d1bc738f767726b91e6acea
Nicolas Carrica <nicolas.carrica@gmail.com>
2025-07-07 12:20:52 -0300

    feat: implementa enum de errores personalizados

--- types/mod.rs
+++ types/mod.rs
@@ -3,9 +3,13 @@ pub mod orden;
 pub mod producto;
 pub mod publicacion;
 pub mod usuario;
+pub mod errores;
+pub mod calificacion;
 
 pub use enums::*;
 pub use orden::*;
 pub use producto::*;
 pub use publicacion::*;
 pub use usuario::*;
+pub use errores::*;
+pub use calificacion::*;
