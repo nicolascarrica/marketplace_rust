65b4ef38b989330a112dcd8a2c79de7d0bac5f9f
gregfc95 <josegfc95@gmail.com>
2025-07-12 14:34:42 -0300

    remover cancelacione

--- types/mod.rs
+++ types/mod.rs
@@ -1,15 +1,13 @@
 pub mod enums;
+pub mod errores;
 pub mod orden;
 pub mod producto;
 pub mod publicacion;
 pub mod usuario;
-pub mod errores;
-pub mod calificacion;
 
 pub use enums::*;
+pub use errores::*;
 pub use orden::*;
 pub use producto::*;
 pub use publicacion::*;
 pub use usuario::*;
-pub use errores::*;
-pub use calificacion::*;
